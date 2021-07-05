70299d988e915cb107e911fdb01cff9fa3b03729
Amaan <azafar2000@gmail.com>
2021-07-05 20:24:08 +0530

    HomeScreen with 1 video

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:video_player_chewie/video_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Chewie Player'),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: [
+            VideoItems(
+                videoPlayerController:
+                    VideoPlayerController.asset('assets/videos/video.mp4'),
+                looping: true,
+                autoplay: true)
+          ],
+        ));
   }
 }
